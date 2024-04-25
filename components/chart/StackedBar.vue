ff494de625153c66cc79f9da2c39bb3a3e364646
219031 <219031@domfam.com>
2024-04-25 13:48:37 +0900

    캐러셀 테이블

--- components/chart/StackedBar.vue
+++ components/chart/StackedBar.vue
@@ -1,150 +1,70 @@
 <template>
-    <div class="stackedColumn">
-        <div class="chart" style="width: 100%">
-            <canvas ref="chart" width="100%" height="49.3rem"></canvas>
-            <div class="legend d-flex">
-                <div class="understand">
-                    <span class="bullet"></span>
-                    이해
-                </div>
-                <div class="listen">
-                    <span class="bullet"></span>
-                    듣기
-                </div>
-                <div class="talk">
-                    <span class="bullet"></span>
-                    말하기
+    <div class="stackedBar">
+        <p class="tit bullet mgt30">각 색깔별 학업 성취율 그룹의 숫자를 누르면 자세한 학습자 정보를 확인할 수 있어요.</p>
+        <div class="colorGroup mgt30">
+            <div class="result_distribution">
+                <!-- width 값은 max-width: 1100값을 기준으로 백분률 값 -->
+                <div
+                    v-for="(count, item, index) in lessonState"
+                    :key="index"
+                    :class="'learner' + (6 - index)"
+                    :style="{ width: calculateWidth(count) + '%' }"
+                >
+                    <div class="count_box" @click="handleClickColorGroup(item)">
+                        <i class="ico learner6"></i>
+                        <span>{{ count }}</span>
+                    </div>
                 </div>
             </div>
+            <v-list class="mgt30 d-flex justify-end" bg-color="transparent" density="compact">
+                <v-list-item v-for="(learner, index) in learners" :key="index"
+                    ><em :class="`shchme-color-learner${learner.level}`" />{{ learner.text }}</v-list-item
+                >
+            </v-list>
         </div>
     </div>
 </template>
 
 <script setup>
-const { $Chart } = useNuxtApp();
-const chart = ref(document.getElementsByTagName('canvas'));
+const learnStore = useApiLearnStore();
+const lessonStore = useApiLessonStore();
+const { lessonState, lessonCommonState } = storeToRefs(lessonStore);
+const learners = [
+    { level: 6, text: '미학습' },
+    { level: 5, text: '느린 학습자' },
+    { level: 4, text: '조금 느린 학습자' },
+    { level: 3, text: '중간 학습자' },
+    { level: 2, text: '조금 빠른 학습자' },
+    { level: 1, text: '빠른 학습자' }
+];
 
-const chartData = ref({
-    labels: ['Lesson 1', 'Lesson 2', 'Lesson 3', 'Lesson 4', 'Lesson 5', 'Lesson 6'],
-    datasets: [
-        {
-            type: 'bar',
-            label: '이해',
-            data: [3, 11, 6, 10, 10, 13],
-            backgroundColor: '#46A7E5',
-            stack: 'word',
-            categoryPercentage: 1.0,
-            barThickness: 65
-        },
-        {
-            type: 'bar',
-            label: '듣기',
-            data: [3, 11, 6, 10, 10, 13],
-            backgroundColor: '#FD6E7F',
-            stack: 'word',
-            categoryPercentage: 1.0,
-            barThickness: 65
-        },
-        {
-            type: 'bar',
-            label: '말하기',
-            data: [3, 11, 6, 10, 10, 13],
-            backgroundColor: '#8DBB2C',
-            stack: 'word',
-            categoryPercentage: 1.0,
-            barThickness: 65
-        }
-    ]
-});
-
-const chartOptions = ref({
-    chartArea: {
-        backgroundColor: '#F8F8F8'
-    },
-    barThickness: 10, // 막대의 너비 조절
-    categorySpacing: 3,
-    scales: {
-        x: {
-            grid: {
-                display: false
-            },
-            ticks: {
-                font: {
-                    weight: '500',
-                    size: 20,
-                    color: '#171717',
-                    family: '"NotoSansKR", sans-serif',
-                    lineHeight: '26px'
-                }
-            }
-        },
-        y: {
-            border: {
-                display: false
-            },
-
-            ticks: {
-                font: {
-                    weight: '500',
-                    size: 16,
-                    color: '#171717',
-                    family: '"NotoSansKR", sans-serif',
-                    lineHeight: '22px'
-                }
-            },
-            suggestedMin: 0,
-            suggestedMax: 60 //max 게이지
-        }
-    },
-    plugins: {
-        datalabels: {
-            display: false
-        },
-        chartArea: {
-            backgroundColor: 'rgba(251, 85, 85, 0.4)'
-        },
-        // 위치 고정으로 인한 별도 작업
-        legend: {
-            display: false,
-            labels: {
-                font: {
-                    weight: '500'
-                }
-            },
-            onClick: null,
-            position: 'bottom'
-        }
+/**
+ * 컬러 그룹 선택 시 itme 저장 및 성취율 테이블 열기
+ * @param item 컬러 그룹
+ * TODO: 컬러 그룹 파라미터 적용 필요
+ */
+const handleClickColorGroup = async item => {
+    lessonCommonState.value.isExpand = true;
+    lessonCommonState.value.selectedColorGroup = item;
+    // 학업성취율 탭에서 컬러그룹 선택 시
+    if (lessonCommonState.value.tab === 'one') {
+        // 컬러그룹에 맞는 성취율 테이블 API 호출
+        await learnStore.getLearnColorMatchedStudentGroups({ studUuid: 11, semId: 22, sessId: 33, chId: 44 });
     }
-});
-
-const plugin = {
-    id: 'custom_canvas_background_color',
-    beforeDraw: (chart, args, options) => {
-        if (chart.options.chartArea && chart.options.chartArea.backgroundColor) {
-            var ctx = chart.ctx;
-            var chartArea = chart.chartArea;
-            var values = chart.data.datasets[0].data;
-            var rowCount = Math.ceil(Math.max.apply(null, values) / 2.2);
-            var width = chartArea.right - chartArea.left;
-            var height = chartArea.bottom - chartArea.top;
-            var rowHeight = height / rowCount;
-            ctx.save();
-            ctx.fillStyle = chart.options.chartArea.backgroundColor;
-            var startPoint = chartArea.top + rowHeight;
-            while (startPoint < chartArea.bottom) {
-                ctx.fillRect(chartArea.left, startPoint, width, rowHeight);
-                startPoint += rowHeight * 2;
-            }
-            ctx.restore();
-        }
+    // TODO: 단원별 누적 성취율 탭에서 컬러그룹 선택 시
+    if (lessonCommonState.value.tab === 'two') {
+        // 컬러그룹에 맞는 단원별 누적 성취율 테이블 API 호출
+        await lessonStore.getUnitAcademicAchievementRate();
     }
 };
 
-onMounted(() => {
-    new $Chart(chart.value, {
-        data: chartData.value,
-        options: chartOptions.value,
-        plugins: [plugin]
-    });
-});
+/**
+ * 색깔별 그룹 width 퍼센트 계산
+ * @param count
+ * @returns {number}
+ */
+const calculateWidth = count => {
+    const totalCount = Object.values(lessonState.value).reduce((acc, cur) => acc + cur, 0);
+    return (count / totalCount) * 100;
+};
 </script>
--- components/table/Carousel.vue
+++ components/table/Carousel.vue
@@ -1,197 +1,252 @@
 <template>
-    <div class="center-table">
-        <!-- 스크롤 계속 되는걸 막을 시 추가 :continuous="false" -->
-        <v-carousel hide-delimiters class="table-carousel" height="auto">
-            <!-- 아이템1 -->
-            <v-carousel-item>
-                <v-data-table :headers="tableUnitHead" :items="tableContent" item-value="number" class="color_gray type_center">
-                    <template v-slot:item="{ item }">
-                        <tr>
-                            <td>
-                                <!-- 차시가 진행되지 않았을 경우 삼각 엠블럼 노출 X -->
-                                <!-- 
-                                    .emblem_01 미학습,
-                                    .emblem_02 느린 학습자,
-                                    .emblem_03 조금 느린 학습자,
-                                    .emblem_03 중간 학습자,
-                                    .emblem_04 조금 빠른 학습자,
-                                    .emblem_05 빠른 학습자
-                                -->
-                                <i class="emblem emblem_02"></i>
-                                <v-btn variant="text" :ripple="false" :class="getClass(item.unit01)">{{
-                                    addPercentIfNumber(item.unit01)
-                                }}</v-btn>
+    <Carousel :breakpoints="breakpoints" class="carousel_mid">
+        <!-- TODO : 10은 총 단원 수 하드코딩 - 변경 필요-->
+        <Slide v-for="(slide, index) in 10" :key="slide.id">
+            <v-data-table
+                :headers="tableUnitHead.slice(index, index + 1)"
+                :items="tableContent.slice(0, props.currentPageCount)"
+                item-value="number"
+                class="color_gray type_center carousel__item"
+                disable-sort
+            >
+                <template v-slot:item="{ item }">
+                    <tr class="carousel_mid_tr">
+                        <td :class="getDisableClass(item[`unit${String(index + 1).padStart(2, '0')}`])">
+                            <!--  .emblem_01 미학습,
+                                  .emblem_02 느린 학습자,
+                                  .emblem_03 조금 느린 학습자,
+                                  .emblem_03 중간 학습자,
+                                  .emblem_04 조금 빠른 학습자,
+                                  .emblem_05 빠른 학습자
+                            -->
 
-                                <!-- 상황에 맞체 화면에 icon 노출 필요  -->
-                                <!-- 출제완료 -->
-                                <i class="ico stamp_complete"></i>
-                                <!-- 출제대기 -->
-                                <!-- <i class="ico stamp_wait"></i> -->
-                            </td>
-                            <td>
-                                <i class="emblem emblem_05"></i>
-                                <v-btn variant="text" :ripple="false" :class="getClass(item.unit02)">{{
-                                    addPercentIfNumber(item.unit02)
-                                }}</v-btn>
-                                <i class="ico list"></i>
-                            </td>
-                            <td>
-                                <i class="emblem" :class="item.unit03 === '미학습' ? 'emblem_01' : 'emblem_04'"></i>
-                                <v-btn variant="text" :ripple="false" :class="getClass(item.unit03)">{{
-                                    addPercentIfNumber(item.unit03)
-                                }}</v-btn>
-                            </td>
-                            <!-- 단원이 진행되지 않았을 경우 .disabled -->
-                            <td class="disabled">
-                                <span :class="getClass(item.unit04)">{{ item.unit04 }}</span>
-                            </td>
-                            <td class="disabled">
-                                <span :class="getClass(item.unit05)">{{ item.unit05 }}</span>
-                            </td>
-                            <td class="disabled">
-                                <span :class="getClass(item.unit06)">{{ item.unit06 }}</span>
-                            </td>
-                        </tr>
-                    </template>
-                </v-data-table>
-            </v-carousel-item>
-            <!-- 아이템2 -->
-            <v-carousel-item>
-                <v-data-table :headers="tableUnitHead" :items="tableContent" item-value="number" class="color_gray type_center">
-                    <template v-slot:item="{ item }">
-                        <tr>
-                            <td>
-                                <!-- 차시가 진행되지 않았을 경우 삼각 엠블럼 노출 X -->
-                                <!-- 
-                                    .emblem_01 미학습,
-                                    .emblem_02 느린 학습자,
-                                    .emblem_03 조금 느린 학습자,
-                                    .emblem_03 중간 학습자,
-                                    .emblem_04 조금 빠른 학습자,
-                                    .emblem_05 빠른 학습자
-                                -->
-                                <i class="emblem emblem_02"></i>
-                                <v-btn variant="text" :ripple="false" :class="getClass(item.unit01)">{{
-                                    addPercentIfNumber(item.unit01)
-                                }}</v-btn>
+                            <i
+                                class="emblem"
+                                :class="{
+                                    emblem_04:
+                                        item[`unit${String(index + 1).padStart(2, '0')}`]?.status === 4 ||
+                                        item[`unit${String(index + 1).padStart(2, '0')}`]?.status === 6,
+                                    emblem_05: item[`unit${String(index + 1).padStart(2, '0')}`]?.status === 5,
+                                    emblem_03: item[`unit${String(index + 1).padStart(2, '0')}`]?.status === 3,
+                                    emblem_02: item[`unit${String(index + 1).padStart(2, '0')}`]?.status === 2,
+                                    emblem_01: item[`unit${String(index + 1).padStart(2, '0')}`]?.status === 1
+                                }"
+                                v-if="item[`unit${String(index + 1).padStart(2, '0')}`]"
+                            />
+                            <div class="carousel_mid_tr_wrap_btn">
+                                <v-btn
+                                    v-if="item[`unit${String(index + 1).padStart(2, '0')}`]"
+                                    variant="text"
+                                    :ripple="false"
+                                    size="small"
+                                    :class="getClass(item[`unit${String(index + 1).padStart(2, '0')}`])"
+                                >
+                                    <span v-if="item[`unit${String(index + 1).padStart(2, '0')}`]?.status !== 1">{{
+                                        addPercentIfNumber(item[`unit${String(index + 1).padStart(2, '0')}`])
+                                    }}</span>
+                                    <span v-else-if="item[`unit${String(index + 1).padStart(2, '0')}`]?.status === 1">미학습</span>
+                                </v-btn>
+                                <i
+                                    v-if="item[`unit${String(index + 1).padStart(2, '0')}`]"
+                                    class="ico"
+                                    :class="{
+                                        stamp_complete: item[`unit${String(index + 1).padStart(2, '0')}`]?.stamp === 1,
+                                        stamp_wait: item[`unit${String(index + 1).padStart(2, '0')}`]?.stamp === 2
+                                    }"
+                                />
+                                <i class="ico list" v-if="item[`unit${String(index + 1).padStart(2, '0')}`]?.status === 6" />
+                                <span v-else-if="item[`unit${String(index + 1).padStart(2, '0')}`] === undefined">학습대기</span>
+                            </div>
+                        </td>
+                    </tr>
+                </template>
+            </v-data-table>
+        </Slide>
 
-                                <!-- 상황에 맞체 화면에 icon 노출 필요  -->
-                                <!-- 출제완료 -->
-                                <i class="ico stamp_complete"></i>
-                                <!-- 출제대기 -->
-                                <i class="ico stamp_wait"></i>
-                            </td>
-                            <td>
-                                <i class="emblem emblem_05"></i>
-                                <v-btn variant="text" :ripple="false" :class="getClass(item.unit02)">{{
-                                    addPercentIfNumber(item.unit02)
-                                }}</v-btn>
-                                <i class="ico list"></i>
-                            </td>
-                            <td>
-                                <i class="emblem" :class="item.unit03 === '미학습' ? 'emblem_01' : 'emblem_04'"></i>
-                                <v-btn variant="text" :ripple="false" :class="getClass(item.unit03)">{{
-                                    addPercentIfNumber(item.unit03)
-                                }}</v-btn>
-                            </td>
-                            <!-- 단원이 진행되지 않았을 경우 .disabled -->
-                            <td class="disabled">
-                                <span :class="getClass(item.unit04)">{{ item.unit04 }}</span>
-                            </td>
-                            <td class="disabled">
-                                <span :class="getClass(item.unit05)">{{ item.unit05 }}</span>
-                            </td>
-                            <td class="disabled">
-                                <span :class="getClass(item.unit06)">{{ item.unit06 }}</span>
-                            </td>
-                        </tr>
-                    </template>
-                </v-data-table>
-            </v-carousel-item>
-        </v-carousel>
-    </div>
+        <template #addons>
+            <Navigation />
+        </template>
+    </Carousel>
 </template>
 
 <script setup>
-// 캐러셀영역
-const tableUnitHead = [
-    {
-        title: '1단원',
-        sortable: false,
-        key: 'unit01'
-    },
-    {
-        title: '2단원',
-        sortable: false,
-        key: 'unit02'
-    },
-    {
-        title: '3단원',
-        sortable: false,
-        key: 'unit03'
-    },
-    {
-        title: '4단원',
-        sortable: false,
-        key: 'unit04'
-    },
-    {
-        title: '5단원',
-        sortable: false,
-        key: 'unit05'
-    },
-    {
-        title: '6단원',
-        sortable: false,
-        key: 'unit06'
+import 'vue3-carousel/dist/carousel.css';
+import { Carousel, Navigation, Slide } from 'vue3-carousel';
+
+const breakpoints = {
+    // // 700px and up
+    // 800: {
+    //     itemsToShow: 2,
+    //     snapAlign: 'center'
+    // },
+    // 1024 and up
+    800: {
+        itemsToShow: 6,
+        snapAlign: 'start'
     }
-];
+};
+
+const props = defineProps(['lessonAccumulatedState', 'currentPageCount']);
+// 캐러셀영역
+
+/**
+ * 동적으로 head 렌더링
+ * @type {{sortable: boolean, title: string, key: string}[]}
+ */
+const tableUnitHead = Array.from({ length: 10 }, (_, index) => ({
+    title: `${index + 1}단원`,
+    sortable: false,
+    key: `unit${String(index + 1).padStart(2, '0')}`
+}));
+
+// 캐러셀 테이블 다음과 같은 형태로 데이터 컨버팅 필요 (lessonAccumulatedState)
 const tableContent = [
     {
-        unit01: 90,
-        unit02: 90,
-        unit03: 90,
-        unit04: '학습대기',
-        unit05: '학습대기',
-        unit06: '학습대기'
+        unit01: {
+            percent: 91,
+            status: 5,
+            stamp: 1
+        },
+        unit02: {
+            percent: 92,
+            status: 6,
+            stamp: 3
+        },
+        unit03: {
+            percent: 93,
+            status: 1,
+            stamp: 2
+        }
     },
     {
-        unit01: 90,
-        unit02: 90,
-        unit03: 90,
-        unit04: '학습대기',
-        unit05: '학습대기',
-        unit06: '학습대기'
+        unit01: {
+            percent: 91,
+            status: 5,
+            stamp: 1
+        },
+        unit02: {
+            percent: 92,
+            status: 6,
+            stamp: 3
+        },
+        unit03: {
+            percent: 93,
+            status: 2,
+            stamp: 2
+        }
     },
     {
-        unit01: 90,
-        unit02: 90,
-        unit03: 90,
-        unit04: '학습대기',
-        unit05: '학습대기',
-        unit06: '학습대기'
+        unit01: {
+            percent: 91,
+            status: 5,
+            stamp: 1
+        },
+        unit02: {
+            percent: 92,
+            status: 6,
+            stamp: 3
+        },
+        unit03: {
+            percent: 93,
+            status: 1,
+            stamp: 2
+        }
     },
     {
-        unit01: 90,
-        unit02: 90,
-        unit03: '미학습',
-        unit04: '학습대기',
-        unit05: '학습대기',
-        unit06: '학습대기'
+        unit01: {
+            percent: 91,
+            status: 5,
+            stamp: 1
+        },
+        unit02: {
+            percent: 92,
+            status: 6,
+            stamp: 3
+        },
+        unit03: {
+            percent: 93,
+            status: 1,
+            stamp: 2
+        }
     },
     {
-        unit01: 90,
-        unit02: 90,
-        unit03: 90,
-        unit04: '학습대기',
-        unit05: '학습대기',
-        unit06: '학습대기'
+        unit01: {
+            percent: 91,
+            status: 5,
+            stamp: 1
+        },
+        unit02: {
+            percent: 92,
+            status: 6,
+            stamp: 3
+        },
+        unit03: {
+            percent: 93,
+            status: 1,
+            stamp: 2
+        }
+    },
+    {
+        unit01: {
+            percent: 91,
+            status: 5,
+            stamp: 1
+        },
+        unit02: {
+            percent: 92,
+            status: 6,
+            stamp: 3
+        },
+        unit03: {
+            percent: 93,
+            status: 1,
+            stamp: 2
+        }
     }
 ];
 
 // 숫자일경우 under_Line 글자인경우 X
-const getClass = value => (typeof value === 'number' ? 'type_default_underline' : 'txt');
+const getClass = value => {
+    if (value) {
+        return typeof value.percent === 'number' ? 'type_default_underline' : 'txt';
+    }
+};
+// 숫자일경우 disabled
+const getDisableClass = value => {
+    if (value) {
+        if (typeof value.percent === 'number') {
+            return '';
+        }
+    } else {
+        return 'disabled';
+    }
+};
 
 // 숫자일경우 %추가
-const addPercentIfNumber = value => (typeof value === 'number' ? `${value}%` : value);
+const addPercentIfNumber = value => {
+    if (value) {
+        if (typeof value.percent === 'number') {
+            return `${value.percent}%`;
+        }
+    } else {
+        return '학습대기';
+    }
+};
 </script>
+
+<style scoped>
+/* 캐러셀 테이블 전체 width */
+.carousel_mid {
+    width: 70%;
+}
+
+/* tr direction 상하 방향으로 변경  */
+.carousel_mid_tr {
+    display: flex;
+    flex-direction: column;
+}
+
+.carousel_mid_tr_wrap_btn {
+    display: flex;
+}
+</style>
