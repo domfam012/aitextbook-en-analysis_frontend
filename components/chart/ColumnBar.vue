0f787a60c6a6f662c7161eb3fc6ba6c7db757558
donggeonseo <donggeon.seo@domfam.com>
2024-04-29 10:13:23 +0900

    학습 정서 그래프 수정

--- components/chart/ColumnBar.vue
+++ components/chart/ColumnBar.vue
@@ -4,7 +4,7 @@
     </div>
     <div class="columnBar mgt15">
         <div class="chart">
-            <Bar :data="emotionChart" :options="chartOptions"></Bar>
+            <Bar :data="emotionChart" :options="chartOptions" :plugins="[plugin]"></Bar>
         </div>
     </div>
 </template>
@@ -100,6 +100,7 @@ const plugin = {
             var ctx = chart.ctx;
             var chartArea = chart.chartArea;
             var values = chart.data.datasets[0]?.data;
+            console.log(values);
             // var rowCount = Math.ceil(Math.max.apply(null, values) / 0.9);
             var rowCount = 6;
             var width = chartArea.right - chartArea.left;
